0e495845037cbf14860f611287dffee27c0701c8
Muhammad Haikal Al Rasyid <haikalslipi@gmail.com>
2024-07-10 18:11:02 +0700

    Add files via upload

--- main.py
+++ main.py
@@ -1,42 +1,18 @@
-from fastapi import FastAPI, File, Form, HTTPException, UploadFile, Query
+from fastapi import FastAPI, Form, HTTPException, UploadFile, Query
 from marshal import loads
-from mpstemmer import MPStemmer
-from numpy import argsort
-from polars import read_csv, Series
-from pydantic import BaseModel, conint
+from polars import read_csv
 from PyPDF2 import PdfReader
-from re import sub
-from Sastrawi.StopWordRemover.StopWordRemoverFactory import StopWordRemoverFactory
 from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.metrics.pairwise import cosine_similarity
 from types import FunctionType
-from typing import List
 
-# Read the dataset from a CSV file
 df = read_csv("temp/repository_pnj_20212023clean.csv")
-
-# Initialize a TF-IDF vectorizer
 vectorizer = TfidfVectorizer()
-
-# Fit the TF-IDF model to the 'f' column of the dataset
 tfidf_matrix = vectorizer.fit_transform(df["f"].to_list())
 
-# Get model from a marshal file
 with open("temp/model.marshall", "rb") as file:
     model = file.read()
 
 app = FastAPI()
-
-
-class TextRequest(BaseModel):
-    title: str
-    abstract: str
-    top_n: int
-
-
-class PDFRequest(BaseModel):
-    file: UploadFile
-    top_n: int
 
 
 @app.get("/")
@@ -44,35 +20,31 @@ async def get_root():
     return {"message": "Scientia-API by Kelompok 4 TI 6A"}
 
 
-@app.post("/find_similar")
-def post_find_similar(request: TextRequest):
+@app.get("/find_similar")
+async def get_find_similar(
+    title: str = Query(...),
+    abstract: str = Query(...),
+    top_n: int = Query(...),
+):
     try:
-        if request.top_n <= 0 or request.top_n > df.shape[0]:
-            raise HTTPException(
-                status_code=400, detail=f"top_n must be between 1 and {df.shape[0]}"
-            )
-
         return FunctionType(loads(model), globals())(
-            request.title, request.abstract, request.top_n
+            title,
+            abstract,
+            top_n,
         ).to_dicts()
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
 
 
 @app.post("/find_similar_pdf")
-def post_find_similar_pdf(file: UploadFile, top_n: int = Form(...)):
+async def post_find_similar_pdf(file: UploadFile, top_n: int = Form(...)):
+    pages = PdfReader(file.file).pages
+
     try:
-        text = PdfReader(file.file).pages[0].extract_text()
-
-        lines = text.split("\n")
-        title = lines[0]
-        abstract = " ".join(lines[1:])
-
-        if top_n <= 0 or top_n > df.shape[0]:
-            raise HTTPException(
-                status_code=400, detail=f"top_n must be between 1 and {df.shape[0]}"
-            )
-
-        return FunctionType(loads(model), globals())(title, abstract, top_n).to_dicts()
+        return FunctionType(loads(model), globals())(
+            pages[0].extract_text(),
+            " ".join([page.extract_text() for page in pages[1:]]),
+            top_n,
+        ).to_dicts()
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
